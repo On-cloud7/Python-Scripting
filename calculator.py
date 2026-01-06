cd9dd2a22bbe7048eb76be5bd276b84c65753069
On-cloud7 <priyanka.p62003@gmail.com>
2026-01-06 22:52:32 +0530

    files added

--- calculator.py
+++ calculator.py
@@ -1,8 +1,4 @@
-num1 = int(input ("enter 1 :"))
-num2 = int(input ("enter 2 :"))
-sum_num = num1 + num2
-print(type(num1))
-print(type(num2))
+import new_calculator as basic_calc
 
-print(sum_num)
 
+basic_calc
---
+++ new_calculator.py
@@ -0,0 +1,20 @@
+num1 = 10
+num = 20
+
+def addition():
+    add = num1+ num
+    print(add)
+
+def sub():
+    sub = num1-num
+    print(sub)
+    
+def mul():
+    mul = num1* num
+    print(mul)
+    
+addition()
+sub()
+mul()
+
+
